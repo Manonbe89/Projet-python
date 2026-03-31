980130fce33660f28d82ca545c02721cbd781ff2
superkik15 <chanudetkylian@gmail.com>
2026-03-31 18:04:31 +0200

    update

--- code/Tile.py
+++ code/Tile.py
@@ -4,26 +4,32 @@ import Wall
 class Tile(pygame.sprite.Sprite):
     def __init__(self, groups, surf):
         super().__init__(groups)
+
         self.enters = {}
         self.objects = {}
         self.teleporters = {}
         self.tile_map = surf
-        self.screen = pygame.display.set_mode((1000, 500))
 
     def _add_enter(self, x, y, name):
         self.enters[name] = (x, y)
 
-    def _add_object(self, name, obj, x, y):
-        obj.rect.topleft = (x, y)
-        self.objects[name] = obj
+    def _add_object(self, name, x, y, surf):
+        #Création du mur
+        wall = Wall.Wall((x, y), surf, self.collision_sprites)
+
+        #Création d’un nom unique basé sur le nom + coordonnées
+        key = f"{name}_{x}_{y}"
+
+        #Stockage dans les objets
+        self.objects[key] = wall
 
     def _add_teleporter(self, name, teleporter, x, y):
         teleporter.rect.topleft = (x, y)
         self.teleporters[name] = teleporter
 
-    def _charge(self):
+    def _draw(self, screen):
         # Affiche la map
-        self.screen.blit(self.tile_map, (0, 0))
+        screen.blit(self.tile_map, (0, 0))
 
         # Affiche chaque objet
         for obj in self.objects.values():
