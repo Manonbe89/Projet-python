f4782a35ac75698fe291670c66fac3370754e01b
superkik15 <chanudetkylian@gmail.com>
2026-04-02 13:57:27 +0200

    update

--- camera.py
+++ camera.py
@@ -1,7 +1,17 @@
+import pygame
+import config
+import player
+
 class Camera : 
 
     def __init__(self):
         self.x_cam = 0
         self.y_cam = 0
+        self.x_min = 0
+        self.y_min = 0
+        self.x_max = 0
+        self.y_max = 0
 
-    def _update_cam(self, height, ):
+    def _update_cam(self, scree_height, screen_widht):
+        if self.x_cam > self.x_min and self.x_cam < self.x_max:
+            x_cam = 0
