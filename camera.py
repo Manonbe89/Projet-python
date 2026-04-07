875d2fd5e1f26a48c17e3642fd3dac950f3b59ec
superkik15 <chanudetkylian@gmail.com>
2026-04-07 13:09:17 +0200

    camera fonctionelle

--- camera.py
+++ camera.py
@@ -1,19 +1,25 @@
 import pygame
 import config
 
-class Camera : 
+class Camera:
+    def __init__(self, screen_width, screen_height):
+        self.position = pygame.math.Vector2()
+        self.screen_width = screen_width
+        self.screen_height = screen_height
 
-    def __init__(self):
-        self.x_cam = 0
-        self.y_cam = 0
-        self.x_min = 0
-        self.y_min = 0
-        self.x_max = 800
-        self.y_max = 800
+        # limites de la map
+        self.map_width = 1000
+        self.map_height = 1000
 
-    def _update_cam(self, scree_height, screen_widht, x_player, y_player):
-        if x_player > self.x_min and x_player < self.x_max:
-            self.x_cam = x_player + screen_widht /2
+    def update(self, player):
+        # centre la caméra sur le joueur
+        self.position.x = player.rect.centerx - self.screen_width // 2
+        self.position.y = player.rect.centery - self.screen_height // 2
 
-        if y_player > self.y_min and y_player < self.y_max:
-            self.y_cam = y_player + scree_height /2
+        # empêche la caméra de sortir de la map
+        self.position.x = max(0, min(self.position.x, self.map_width - self.screen_width))
+        self.position.y = max(0, min(self.position.y, self.map_height - self.screen_height))
+
+    def apply(self, rect):
+        # retourne un rect décalé par la caméra
+        return rect.move(-self.position.x, -self.position.y)
