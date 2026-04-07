b10f0f821fe6d0af368de227c11e3da485b601b9
superkik15 <chanudetkylian@gmail.com>
2026-04-07 16:39:16 +0200

    commentaire camera

--- camera.py
+++ camera.py
@@ -1,13 +1,13 @@
 import pygame
-import config
 
+# Classe de calcul de la camera
 class Camera:
     def __init__(self, screen_width, screen_height):
-        self.position = pygame.math.Vector2()
+        self.position = pygame.math.Vector2()   # décalage de la caméra sur la map
         self.screen_width = screen_width
         self.screen_height = screen_height
 
-        # limites de la map
+        # limites de la map (peut etre à mettre en parametre si on fait des maps de taille différentes)
         self.map_width = 1000
         self.map_height = 1000
 
@@ -16,7 +16,10 @@ class Camera:
         self.position.x = player.rect.centerx - self.screen_width // 2
         self.position.y = player.rect.centery - self.screen_height // 2
 
-        # empêche la caméra de sortir de la map
+        # empêche la caméra de sortir de la map en prenant comme décalage : 
+        # - le min de la limite supérieur et de la position du décalage (empeche d'etre plus grand que la limite supérieur)
+        # - le max entre la limite inférieur et la position du décalage (empèche d'etre plus petit que la limite inférieur)
+        # rmq : si la limite supérieur est prise au premier min on auras max(limite supérieur, 0 (limite inférieur)) ce qui donneras toujours la limite supérieur : c'est cohérent
         self.position.x = max(0, min(self.position.x, self.map_width - self.screen_width))
         self.position.y = max(0, min(self.position.y, self.map_height - self.screen_height))
 
