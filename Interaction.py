4f62899f0ea1e294d65167faa48a347833b7b290
superkik15 <chanudetkylian@gmail.com>
2026-04-12 13:00:08 +0200

    ajout classe d'interaction (pour l'instant avec des npc)

---
+++ Interaction.py
@@ -0,0 +1,29 @@
+import pygame
+import Player
+
+class Interaction:
+    def __init__(self, player):
+        self.player = player
+        self.last_space_action = False
+        self.in_action = False
+
+    def _interact_npc(self, npc_group, screen, font):
+        space_action = self.player.game.actions['space']
+        space = space_action and not self.last_space_action
+        
+        npc = self._search_npc(npc_group)
+        
+        if self.in_action :
+            if space :
+                self.in_action = False
+
+            else :
+                if npc is not None:
+                    npc._show_quote(screen, font)
+
+        else :
+            if npc is not None and space:
+                self.in_action = True
+                npc._show_quote(screen, font)
+
+        self.last_space_action = space_action
