d2aa3eb48d6db0e66ddf876bf2b90c737a4dde0e
Manon <manon.bettstatter@gmail.com>
2026-04-21 18:06:55 +0200

    Utilisation d'objet fonctionnelle

--- Code/usable_Item.py
+++ Code/usable_Item.py
@@ -11,7 +11,7 @@ class Usable_Item (Item):
         self.name = name
         self.picture = picture
         self.usage = usage
-        self.use_item = False       #booléen qui verifie si on utilise un item
+        self.use_item = False       #booléen qui verifie si on a utilise un item
         self.dialogue_step = 0
 
 
@@ -22,36 +22,40 @@ class Usable_Item (Item):
     def _check_item_status_(self, event):
          if event.type == pygame.KEYDOWN :           # vérifie si l'événement keydown s'est produit ou non
               if event.key == pygame.K_u :           # vérifie si la touche "u" a été pressée
-                   self.use_item = True
                    self.dialogue_step += 1
 
 
     def _Use_Item_(self, player, uitem, screen, font):
-                if self.use_item == True :
-                        if self.dialogue_step == 1 :
-                            screen.blit(font.render("Vous utilisez : " + uitem._get_Name_(), True, (255, 255, 255)), (300, 200))  #affiche le texte
-                            uitem._show_passage_text(1, screen, font)
+            if self.dialogue_step == 1 :
+                screen.blit(font.render("Vous utilisez : " + uitem._get_Name_(), True, (255, 255, 255)), (300, 200))  #affiche le texte
+                uitem._show_passage_text(1, screen, font)
 
-
-                        elif self.dialogue_step == 2 :
+            elif self.dialogue_step == 2 :
                             if uitem._get_Name_() == "épée du voyageur" :
-                                screen.blit(font.render("Vous gagnez 5 points d'attaque", True, (255, 255, 255)), (300, 200)) 
-                                player._set_stat("attack", 5)       #pas fonctionnel
+                                screen.blit(font.render("Vous gagnez 5 points d'attaque", True, (255, 255, 255)), (300, 200))
                                 uitem._show_passage_text(1, screen, font) 
-                        elif self.dialogue_step == 3 :      #pour le debugage
-                                    screen.blit(font.render("Vos points d'attaque : " + str(player._get_stat("attack")), True, (255, 255, 255)), (300, 200)) 
-                                    uitem._show_passage_text(1, screen, font) 
+                                if self.use_item == False : 
+                                      player._set_stat("attack", 5)
+                                      self.use_item = True
 
-                                    if uitem._get_Name_() == "vieux grimoire" : 
-                                        screen.blit(font.render("Vous gagnez 5 points de magie", True, (255, 255, 255)), (300, 200)) 
-                                        uitem._show_passage_text(1, screen, font)
-                                        player._set_stat("magic", 5)
+                                if self.dialogue_step == 3 :
+                                       screen.blit(font.render("Vos points d'attaque : " + str(player._get_stat("attack")), True, (255, 255, 255)), (300, 200)) 
+                                       uitem._show_passage_text(1, screen, font)
 
-                                 
+                            if uitem._get_Name_() == "vieux grimoire" : 
+                                screen.blit(font.render("Vous gagnez 5 points de magie", True, (255, 255, 255)), (300, 200)) 
+                                uitem._show_passage_text(1, screen, font)
+                                if self.use_item == False : 
+                                      player._set_stat("magic", 5)
+                                      self.use_item = True   
 
-                        elif self.dialogue_step >= 4 :
-                             self.use_item = False
+                                if self.dialogue_step == 3 :
+                                       screen.blit(font.render("Vos points de magie : " + str(player._get_stat("attack")), True, (255, 255, 255)), (300, 200)) 
+                                       uitem._show_passage_text(1, screen, font)
+
+            elif self.dialogue_step >= 4 :
                              self.dialogue_step = 0
+
 
     def _show_passage_text(self, number, screen, font):
          if number == 1 :
