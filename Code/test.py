3aa6e7834fd3693e21f334275530420b352099a9
superkik15 <chanudetkylian@gmail.com>
2026-04-22 17:57:17 +0200

    update test

---
+++ Code/test.py
@@ -0,0 +1,92 @@
+import pygame
+from camera import Camera
+from npc import NPC
+from player import Player
+from tile import Tile
+from collision_group import Collision_groups
+from interaction import Inreraction
+
+# PROGRAMME DE TEST
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+game = Game()
+
+all_sprites = pygame.sprite.Group()
+solid_walls = pygame.sprite.Group()
+breakable_walls = pygame.sprite.Group()
+pushable_walls = pygame.sprite.Group()
+npc_group = pygame.sprite.Group()
+
+font = pygame.font.Font(None, 32)
+
+
+# SUPER GROUP
+
+collision_groups = Collision_groups(solid_walls, breakable_walls, pushable_walls, npc_group)
+
+# MAP
+map_surface = pygame.Surface((1000, 1000))
+map_surface.fill((80, 180, 80))
+tile = Tile(map_surface, solid_walls, breakable_walls, pushable_walls, npc_group)
+
+# MUR
+wall_surface = pygame.Surface((100, 100))
+wall_surface.fill((120, 60, 20))
+tile._add_solid_walls("mur", 300, 200, wall_surface)
+
+wall_surface = pygame.Surface((100, 100))
+wall_surface.fill((120, 60, 20))
+tile._add_pushable_walls("mur", 500, 200, wall_surface)
+
+wall_surface = pygame.Surface((100, 100))
+wall_surface.fill((120, 60, 20))
+tile._add_breakable_walls("mur", 300, 500, wall_surface)
+
+# NPC
+npc_surface = pygame.Surface((50, 50))
+npc_surface.fill((255, 0, 0))
+tile._add_npc("Jean", npc_surface, 500, 500, "Boujour Aventurier !")
+
+# JOUEUR
+player = Player((100, 100), "Test", game, all_sprites, collision_groups)
+
+# CAMERA
+camera = Camera(800, 600)
+
+# INTERACTION
+interaction = Inreraction(player)
+
+running = True
+while running:
+    dt = clock.tick(60) / 1000
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # INPUT
+    keys = pygame.key.get_pressed()
+    player.game.actions = {
+        'move up': keys[pygame.K_UP],
+        'move down': keys[pygame.K_DOWN],
+        'move left': keys[pygame.K_LEFT],
+        'move right': keys[pygame.K_RIGHT],
+        'space': keys[pygame.K_SPACE]
+    }
+
+    # UPDATE
+    all_sprites.update(dt)
+    camera.update(player)
+
+    # DRAW
+    tile._draw(screen, camera)
+    screen.blit(player.image, camera.apply(player.rect))
+
+    # INTERACTION
+    interaction._interact_npc(npc_group, screen, font)
+
+    pygame.display.flip()
+
+pygame.quit()
