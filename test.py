d496c1d43a675705fbb24f4f36916eeb78452b4b
superkik15 <chanudetkylian@gmail.com>
2026-04-24 18:17:39 +0200

    modif citation npc test

--- test.py
+++ test.py
@@ -48,7 +48,7 @@ tile._add_breakable_walls("mur", 300, 500, wall_surface)
 # NPC
 npc_surface = pygame.Surface((50, 50))
 npc_surface.fill((255, 0, 0))
-tile._add_npc("Jean", npc_surface, 500, 500, "Vous savez, moi je ne crois pas qu’il y ait de bonne ou de mauvaise situation. Moi, si je devais résumer ma vie aujourd’hui avec vous, je dirais que c’est d’abord des rencontres.")
+tile._add_npc("Numerobis", npc_surface, 500, 500, "Vous savez, moi je ne crois pas qu’il y ait de bonne ou de mauvaise situation. Moi, si je devais résumer ma vie aujourd’hui avec vous, je dirais que c’est d’abord des rencontres. Des gens qui m’ont tendu la main, peut-être à un moment où je ne pouvais pas, où j’étais seul chez moi.")
 
 # JOUEUR
 player = Player((100, 100), "Test", game, all_sprites, collision_groups)
