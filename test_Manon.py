d446f0883051629f0601eaa7029b6a68eebe1727
Manon <manon.bettstatter@gmail.com>
2026-04-27 17:45:26 +0200

    Cohérence du groupe

--- test_Manon.py
+++ test_Manon.py
@@ -1,53 +1,81 @@
 import pygame
-from Code.item.inventory import Inventory
-from Code.player.player import Player
-from Code.item.usable_Item import Usable_Item
-from Code.map.collision_group import Collision_groups
-from Code.map.wall import Wall
-from Code.map.tile import Tile
-from Code.map.camera import Camera
 from Code.game.game import Game
+from Code.item.inventory import Inventory
 from Code.item.item import Item
+from Code.item.usable_Item import Usable_Item
+from Code.map.camera import Camera
+from Code.map.collision_group import Collision_groups
+from Code.map.tile import Tile
+from Code.map.wall import Wall
+from Code.npc.npc import NPC
+from Code.player.player import Player
+from Code.player.interaction import Interaction
 
 pygame.init()
 
 screen = pygame.display.set_mode((900,600))        #définition de la fenêtre  avant 1000 500
 pygame.display.set_caption('jeux')                  #nom de la fenêtre
 clock = pygame.time.Clock()
-
 game = Game()
 inventory = Inventory()
 
 # GROUPES (pas exploité (sauf all_sprites) mais nécessaire pour faire des déplacements)
 all_sprites = pygame.sprite.Group()
-wall_group = pygame.sprite.Group()
 solid_walls = pygame.sprite.Group()
 breakable_walls = pygame.sprite.Group()
 pushable_walls = pygame.sprite.Group()
-collision_groups = Collision_groups(wall_group)
+npc_group = pygame.sprite.Group()
 
-test_font = pygame.font.Font(None, 30)
-text_surface = test_font.render('Bonjour héros', True,'White')
-n=0
+font = pygame.font.Font(None, 32)
 
-# MAP (nécessaire pour une caméra (et donc des déplacements de joueur optimal))
-map_surface = pygame.Surface((4000, 4000))
+# SUPER GROUP
+
+collision_groups = Collision_groups(solid_walls, breakable_walls, pushable_walls, npc_group)
+
+# MAP
+map_surface = pygame.Surface((1000, 1000))
 map_surface.fill((80, 180, 80))
-tile = Tile(map_surface, solid_walls, breakable_walls, pushable_walls)
+tile = Tile(map_surface, solid_walls, breakable_walls, pushable_walls, npc_group)
+
+# MUR
+wall_surface = pygame.Surface((100, 100))
+wall_surface.fill((120, 60, 20))
+tile._add_solid_walls("mur", 300, 200, wall_surface)
+
+wall_surface = pygame.Surface((100, 100))
+wall_surface.fill((120, 60, 20))
+tile._add_pushable_walls("mur", 500, 200, wall_surface)
+
+wall_surface = pygame.Surface((100, 100))
+wall_surface.fill((120, 60, 20))
+tile._add_breakable_walls("mur", 300, 500, wall_surface)
+
+# NPC
+npc_surface = pygame.Surface((50, 50))
+npc_surface.fill((255, 0, 0))
+tile._add_npc("Numerobis", npc_surface, 500, 500, "Vous savez, moi je ne crois pas qu’il y ait de bonne ou de mauvaise situation. Moi, si je devais résumer ma vie aujourd’hui avec vous, je dirais que c’est d’abord des rencontres. Des gens qui m’ont tendu la main, peut-être à un moment où je ne pouvais pas, où j’étais seul chez moi.")
+
+# JOUEUR
+player = Player((100, 100), "Test", game, all_sprites, collision_groups)
+
+# CAMERA
+camera = Camera(800, 600, 1000, 1000)
+
+# INTERACTION
+interaction = Interaction(player)
 
 #ma partie (test)
 uitem = Usable_Item (None, "Rien", None, None, "Images/epee_2.png")
 player = Player ((0,0), "truc", None, all_sprites, collision_groups)
 inventory._item_factory()
 
-
-while True:
+running = True
+while running:
     dt = clock.tick(60) / 1000
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+            running = False
         
         inventory._check_inventory_status(event)
         inventory._check_buttons(event)
@@ -63,16 +91,20 @@ while True:
         'space': keys[pygame.K_SPACE]
     }
 
-    # DEPLACEMENT
-    camera = Camera(4000, 4000)
-    all_sprites.update(dt)
+    # UPDATE
+    all_sprites.update(dt, interaction._get_state())
     camera._update(player)
+
+    # DRAW
     tile._draw(screen, camera)
     screen.blit(player.image, camera._apply(player.rect))
 
-    inventory._display_inventory(screen, test_font)                            #affiche l'inventaire si la condition est respectée
-    uitem._Use_Item_(player, screen, test_font, inventory)
-    screen.blit(test_font.render("Stats : " + 
+    # INTERACTION
+    interaction._interact_npc(npc_group, screen, font)
+
+    inventory._display_inventory(screen, font)                            #affiche l'inventaire si la condition est respectée
+    uitem._Use_Item_(player, screen, font, inventory)
+    screen.blit(font.render("Stats : " + 
                                  "life = " + str(player._get_stat("life")) + " / " +
                                  "attack = " + str(player._get_stat("attack")) + " / " +
                                  "armor = " + str(player._get_stat("armor")) + " / " + 
@@ -81,5 +113,6 @@ while True:
                                  "speed = " + str(player._get_stat("speed"))  
                                  , True, (255, 255, 255)), (5, 550))
 
-    n=n+10
     pygame.display.flip()
+
+pygame.quit()
